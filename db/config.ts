4c8142b8f8e87ed52bb9b39748621a434b4fb634
Takashi Fujita <tgfjt.mail@gmail.com>
2024-03-13 23:28:12 +0900

    db

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,23 @@
-import { defineDb } from 'astro:db';
+import { defineTable, defineDb, column, NOW } from 'astro:db';
 
-// https://astro.build/db/config
+const Post = defineTable({
+  columns: {
+    id: column.number({ primaryKey: true }),
+    title: column.text(),
+    content: column.text(),
+    description: column.text({ optional: true }),
+    category: column.text({ optional: true }),
+    mainImage: column.text({ optional: true }),
+    created: column.date({ default: NOW }),
+    updated: column.date({ default: NOW }),
+    published: column.date({ optional: true }),
+    metadata: column.json({ optional: true}),
+    indexes: {
+      title_idx: { on: ['title'], unique: true },
+    }
+  }
+});
+
 export default defineDb({
-	tables: {},
+  tables: { Post },
 });
